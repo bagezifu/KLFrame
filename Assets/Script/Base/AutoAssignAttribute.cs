650eea338e09677b47f47009a7e36b0ba63d42ab
bagezifu <bagezifu@qq.com>
2016-08-31 05:06:26 +0800

    基础框架

---
+++ Assets/Script/Base/AutoAssignAttribute.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+[AttributeUsage(AttributeTargets.Field|AttributeTargets.Property, AllowMultiple = true)]
+public class AutoAssignAttribute : Attribute
+{
+    public AutoAssignAttribute() {}
+}
---
+++ Assets/Script/Base/InstanceBase.cs
@@ -0,0 +1,14 @@
+﻿//author:kuribayashi  2016年8月7日23:13:28
+using UnityEngine;
+
+public class InstanceBase<T> : MonoBehaviour where T:class
+{
+    private static T instance;
+    public InstanceBase() {
+        instance = this as T;     
+    }
+
+    public static T GetInstance() {
+        return instance;
+    }
+}
