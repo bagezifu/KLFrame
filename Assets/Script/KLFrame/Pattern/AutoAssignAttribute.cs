fdb7b45806f7de41b5a054e2892ffc73d6a71baa
bagezifu <bagezifu@qq.com>
2016-09-02 05:54:08 +0800

    修改自动赋值特性成员

--- Assets/Script/KLFrame/Pattern/AutoAssignAttribute.cs
+++ Assets/Script/KLFrame/Pattern/AutoAssignAttribute.cs
@@ -8,6 +8,10 @@ namespace KLFrame{
     //特性类,为成员变量添加特性,使其获得自动赋值能力
     public class AutoAssignAttribute : Attribute
     {
+        public bool autoAdd { get; set; } 
         public AutoAssignAttribute() { }
+        public AutoAssignAttribute(bool AUTOADD) {
+            autoAdd = AUTOADD;
+        }
     }
 }
