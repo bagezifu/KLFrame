3dc3da28b54936a4fb3a57a0714459772774edd9
bagezifu <bagezifu@qq.com>
2016-09-02 05:55:27 +0800

    新增扩展类

---
+++ Assets/Script/KLFrame/KLExtension.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace KLFrame
+{
+    public static class KLExtension
+    {
+        public static Vector3 GetPosition(this GameObject go) {
+            return go.transform.position;
+        }
+        public static Vector3 SetPosition(this GameObject go,Vector3 pos) {
+           return go.transform.position = pos;
+        }
+        public static void SetParent(this GameObject go,Transform parent) {
+            go.transform.SetParent(parent);
+        }
+        public static void SetParent(this GameObject go, GameObject parent)
+        {
+            go.transform.SetParent(parent.transform);
+        }
+        public static int GetLayer(this Transform tran) {
+            return tran.gameObject.layer;
+        }
+        
+    }
+}
